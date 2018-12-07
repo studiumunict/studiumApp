7b1aa5c12337698f232830b7e0f367d1c5585370
Francesco Petrosino <francy-98_ct@hotmail.it>
2018-12-07 10:39:56 +0100

    Aggiunta extension alla UIColor con nuovi colori

---
+++ Studium/Extension/ExtensionUIColor.swift
@@ -0,0 +1,15 @@
+//
+//  ExtensionUIColor.swift
+//  Studium
+//
+//  Created by Francesco Petrosino on 06/12/2018.
+//  Copyright © 2018 Unict.it. All rights reserved.
+//
+
+extension UIColor{
+    
+    static var sfondo: UIColor = #colorLiteral(red: 0.146052599, green: 0.146084398, blue: 0.146048367, alpha: 1)
+    static var sfondoSecondario: UIColor = #colorLiteral(red: 0.3292481303, green: 0.3293089271, blue: 0.3292401433, alpha: 1)
+    static var titolo: UIColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.0) //a caso
+    
+}
