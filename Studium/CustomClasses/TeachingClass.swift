581ae45a8aadbdc901f2717e312fd45e801d6d5e
Francesco Petrosino <francy-98_ct@hotmail.it>
2018-12-08 15:32:19 +0100

    Aggiornati metodi e proprietà

--- Studium/CustomClasses/TeachingClass.swift
+++ Studium/CustomClasses/TeachingClass.swift
@@ -11,14 +11,13 @@ import Foundation
 class Teaching{
     var name: String!
     var code: Int!
-    var signedUp : Bool!
+    var signedUp : Bool! //TRUE: se sei iscritto, FALSE: altrimenti
     var teacherName: String!
     var haveShowcase: Bool!
-    var haveDescription: Bool!
     var haveDocuments: Bool!
     var haveBooking: Bool!
     var descriptionText: String!
-    //...
+    
     
     init(teachingName: String, teachingCode: Int, teacherName: String, signedUp :Bool) {
         self.name = teachingName
@@ -27,45 +26,11 @@ class Teaching{
         self.signedUp = signedUp
     }
     
-    init(teachingName: String, teachingCode: Int, teacherName: String, haveShowcase: Bool?, haveDescription: Bool?, haveDocuments: Bool?, haveBooking: Bool?, descriptionText: String?){
-        self.name = teachingName;
-        self.code = teachingCode;
-        self.teacherName = teacherName;
+    
+    func completeDataSource(haveShowcase: Bool?, haveDocuments: Bool?, haveBooking: Bool?, descriptionText: String?){
         
         if let val = haveShowcase {
             self.haveShowcase = val
-        }
-        
-        if let val = haveDescription {
-            self.haveDescription = val
-        }
-        
-        if let val = haveDocuments {
-            self.haveDocuments = val
-        }
-        
-        if let val = haveBooking {
-            self.haveBooking = val
-        }
-        
-        if let val = descriptionText {
-            self.descriptionText = val
-        }
-    }
-    
-    
-    func completeDataSource(teacherName: String?, haveShowcase: Bool?, haveDescription: Bool?, haveDocuments: Bool?, haveBooking: Bool?, descriptionText: String?){
-        
-        if let val = teacherName {
-            self.teacherName = val;
-        }
-        
-        if let val = haveShowcase {
-            self.haveShowcase = val
-        }
-        
-        if let val = haveDescription {
-            self.haveDescription = val
         }
         
         if let val = haveDocuments {
