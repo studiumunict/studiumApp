04d1fa34f3ed495d64ad18ff3d5484f247062e5d
Francesco Petrosino <francy-98_ct@hotmail.it>
2018-12-10 01:22:05 +0100

    Prima versione funzionante di UIPageViewController sulla TeachingViewController

---
+++ Studium/TeachingViewController/PagesViewController/DescriptionPageViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DescriptionPageViewController.swift
+//  Studium
+//
+//  Created by Francesco Petrosino on 10/12/2018.
+//  Copyright © 2018 Unict.it. All rights reserved.
+//
+
+import UIKit
+
+class DescriptionPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //self.view.frame = viewAppoggio.frame
+        self.view.backgroundColor = UIColor.blue
+    }
+
+
+}
---
+++ Studium/TeachingViewController/PagesViewController/ShowcasePageViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ShowcasePageViewController.swift
+//  Studium
+//
+//  Created by Francesco Petrosino on 10/12/2018.
+//  Copyright © 2018 Unict.it. All rights reserved.
+//
+
+import UIKit
+
+class ShowcasePageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //self.view.frame = viewAppoggio.frame
+        self.view.backgroundColor = UIColor.red
+    }
+    
+
+}
