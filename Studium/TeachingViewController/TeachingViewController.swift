a346b9ab98d16b011efabd155976d9c8eb0b41b9
Francesco Petrosino <francy-98_ct@hotmail.it>
2018-12-18 15:36:15 +0100

    agg

--- Studium/TeachingViewController/TeachingViewController.swift
+++ Studium/TeachingViewController/TeachingViewController.swift
@@ -231,7 +231,7 @@ class TeachingViewController: UIViewController, UIPageViewControllerDataSource, 
     
     func completeTeachingDataSource(){
         //Scarica i dati dal db
-        teachingDataSource.completeDataSource(showcaseHTML: "https://github.com", syllabusCode: "14927", haveDocuments: false, haveBooking: false, descriptionText: "ciao")
+        teachingDataSource.completeDataSource(showcaseHTML: "https://www.apple.com/it/", syllabusCode: "14927", haveDocuments: false, haveBooking: false, descriptionText: "ciao")
         
         teachingDataSource.addNewNotify(date: "30/10/2018", title: "Date esami", message: "Giorno 2 novembre ci sarà la prima prova scritta.")
         teachingDataSource.addNewNotify(date: "25/11/2018", title: "Lezione rimandata", message: "Si avvisano gli studenti che giorno 26 novembre non ci sarà lezione.")
