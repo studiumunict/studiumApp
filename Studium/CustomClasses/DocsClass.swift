737fcf1a95a273d89803b1fe8d91cc58f834ebe7
Simone Scionti <simoncello98@yahoo.it>
2019-11-10 16:12:57 +0200

    correzione html title documenti

--- Studium/CustomClasses/DocsClass.swift
+++ Studium/CustomClasses/DocsClass.swift
@@ -37,6 +37,30 @@ class Docs {
         self.size = 0
         self.ID = addressOf(self)
     }
+    private func parseHTMLText(text: String)-> String{
+           var str = text.replacingOccurrences(of: "<[^>]+>", with: "", options: .regularExpression, range: nil)
+           str = str.replacingOccurrences(of: "&#39;", with: "'", options: .caseInsensitive, range: nil)
+            str = str.replacingOccurrences(of: "&#039;", with: "'", options: .caseInsensitive, range: nil)
+           str = str.replacingOccurrences(of: "&nbsp;", with: "", options: .caseInsensitive, range: nil)
+           str = str.replacingOccurrences(of: "&quot;", with: "\"", options: .caseInsensitive, range: nil)
+           str = str.replacingOccurrences(of: "&rsquo;", with: "'", options: .caseInsensitive, range: nil)
+           str = str.replacingOccurrences(of: "&agrave;", with: "à", options: .caseInsensitive, range: nil)
+           str = str.replacingOccurrences(of: "&egrave;", with: "è", options: .caseInsensitive, range: nil)
+           str = str.replacingOccurrences(of: "&igrave;", with: "ì", options: .caseInsensitive, range: nil)
+           str = str.replacingOccurrences(of: "&ograve;", with: "ò", options: .caseInsensitive, range: nil)
+           str = str.replacingOccurrences(of: "&ugrave;", with: "ù", options: .caseInsensitive, range: nil)
+           var i = str.count-1
+           var char = false
+           while(i >= 0 && !char){
+               if(str[i] != " "){
+                   char = true
+               }
+               else{i-=1}
+           }
+           let finalStr = String(str.dropLast(str.count-1-i))
+           return finalStr
+       }
+    
     init(title: String, path: String, type: String, uploaded: String, lastUpdate: String, size: Int) {
         self.title = title
         self.path = path
@@ -47,6 +71,7 @@ class Docs {
         self.size = size
         self.CanOpen = true
         self.ID = addressOf(self)
+        self.title = self.parseHTMLText(text: self.title)
         /*if type == .file {
             let dot = path.firstIndex(of: ".")!
             self.title = String(path[..<dot])
